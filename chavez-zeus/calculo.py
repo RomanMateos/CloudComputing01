dba74e07f7f3d19a575867f730abf8ba4d200e86
zeus-chavez <zeuschavez17@gmail.com>
2025-05-03 09:40:24 -0400

    agrego carpeta y saludo en Python

---
+++ chavez-zeus/calculo.py
@@ -0,0 +1,9 @@
+total = int(input("Ingrese el total de la Cuenta"))
+prop = int(input("Ingrese el porcentaje de la propina"))
+
+total_1 = total * prop / 100
+print("Propina", total_1)
+
+cuenta_2 = total_1 + total
+print ("La cuenta en total", cuenta_2)
+
